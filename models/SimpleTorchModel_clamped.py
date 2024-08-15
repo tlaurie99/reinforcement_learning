3cba2f557eca2e0b8bd37a9b63b57855f7fe1a19
Tyler <57071969+tlaurie99@users.noreply.github.com>
2024-08-15 15:40:23 -0400

    Update SimpleTorchModel_clamped.py
    adding overrides to models

--- models/SimpleTorchModel_clamped.py
+++ models/SimpleTorchModel_clamped.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from ray.rllib.models import ModelCatalog
+from ray.rllib.utils.annotations import override
 from ray.rllib.utils.framework import try_import_torch
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
@@ -15,7 +16,7 @@ class SimpleCustomTorchModel(TorchModelV2, nn.Module):
         self.critic_fcnet = TorchFC(obs_space, action_space, 1, model_config, name + "_critic")
         self.actor_fcnet = TorchFC(obs_space, action_space, action_space.shape[0]*2, model_config, name + 
                                    "_actor")
-
+    @override(TorchModelV2)
     def forward(self, input_dict, state, seq_lens):
         # Get the model output
         logits, _ = self.actor_fcnet(input_dict, state, seq_lens)
@@ -29,6 +30,7 @@ class SimpleCustomTorchModel(TorchModelV2, nn.Module):
         self.value, _ = self.critic_fcnet(input_dict, state, seq_lens)
         return logits, state
 
+    @override(TorchModelV2)
     def value_function(self):
         return self.value.squeeze(-1)
 
