dfccbdc9c78a76854d8747be7ad02ec613fbe96a
Tyler <57071969+tlaurie99@users.noreply.github.com>
2024-11-11 10:54:26 -0500

    Update and rename MOG_critic_model.py to MOGCritic.py
    clean up

--- models/MOGCritic.py
+++ models/MOGCritic.py
@@ -7,7 +7,7 @@ from reinforcement_learning.MoG.MoG_module import CriticMoG
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
 
-class CustomTorchModelMOG(TorchModelV2, nn.Module):
+class MOGCritic(TorchModelV2, nn.Module):
     def __init__(self, obs_space, action_space, num_outputs, model_config, name):
         super(CustomTorchModelMOG, self).__init__(obs_space, action_space, num_outputs, model_config, name)
         
@@ -16,9 +16,8 @@ class CustomTorchModelMOG(TorchModelV2, nn.Module):
         torch.autograd.set_detect_anomaly(True)
 
         self.gamma = 0.99
-        self.step_number = 0
         self.activation_fn = model_config['fcnet_activation']
-#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # custom MOG class
         self.critic_network = CriticMoG(obs_space = obs_space, num_gaussians = 3, 
                                         hidden_layer_dims = 256, num_layers = 2, 
                                         activation = self.activation_fn)
@@ -27,29 +26,30 @@ class CustomTorchModelMOG(TorchModelV2, nn.Module):
         
     @override(TorchModelV2)
     def forward(self, input_dict, state, seq_lens):
-        obs = input_dict['obs_flat'].float()
-        batch_size = obs.shape[0]
         # actor forward pass
-        raw_action_logits, _ = self.actor_network(input_dict, state, seq_lens)
+        logits, _ = self.actor_fcnet(input_dict, state, seq_lens)
+        means, log_stds = torch.chunk(logits, 2, -1)
+        # assuming means are normalized between -1 and 1
+        means_clamped = torch.clamp(means, -1, 1)
+        # this is based on the means being -1 to 1 so the std_dev domain would be [0,1)
+        # where exp(-10) and exp(0) would give the above domain for std_dev
+        log_stds_clamped = torch.clamp(log_stds, -10, 0)
+        logits = torch.cat((means_clamped, log_stds_clamped), dim = -1)
         # critic forward pass for MoG network
         self.critic_output, _ = self.critic_network(input_dict, state, seq_lens)
-        self.step_number += 1
-        
-        return raw_action_logits, state
+        return logits, state
 
     @override(TorchModelV2)
     def value_function(self):
         return self.critic_network.value_function()
+        # or the below
+        # return self.critic_output.squeeze(-1)
 
     @override(TorchModelV2)
     def custom_loss(self, policy_loss, sample_batch):
         critic_loss = self.critic_network.custom_loss(sample_batch = sample_batch, gamma=self.gamma)
-        total_loss = [loss + critic_loss for loss in policy_loss]
-        
-        if self.step_number % 1_000 == 0:
-            print(f"policy loss: {policy_loss} enn loss: {total_loss}")
-    
+        total_loss = [loss + critic_loss for loss in policy_loss]    
         return total_loss
 
 
-ModelCatalog.register_custom_model("custom_torch_model_mog", CustomTorchModelMOG)
+ModelCatalog.register_custom_model("MOGCritic", MOGCritic)
