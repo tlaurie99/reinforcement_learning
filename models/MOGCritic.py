696917e6df3ebe99b84f2e758fd92d90c32f3bdf
Tyler <57071969+tlaurie99@users.noreply.github.com>
2024-11-11 10:58:08 -0500

    Update MOGCritic.py
    more clean up

--- models/MOGCritic.py
+++ models/MOGCritic.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from ray.rllib.models import ModelCatalog
 from ray.rllib.utils.annotations import override
 from ray.rllib.utils.framework import try_import_torch
-from reinforcement_learning.MoG.MoG_module import CriticMoG
+from reinforcement_learning.MoG.MOG import MOG
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
 
@@ -18,7 +18,7 @@ class MOGCritic(TorchModelV2, nn.Module):
         self.gamma = 0.99
         self.activation_fn = model_config['fcnet_activation']
         # custom MOG class
-        self.critic_network = CriticMoG(obs_space = obs_space, num_gaussians = 3, 
+        self.critic_network = MOG(obs_space = obs_space, num_gaussians = 3, 
                                         hidden_layer_dims = 256, num_layers = 2, 
                                         activation = self.activation_fn)
         self.actor_network = TorchFC(obs_space, action_space, action_space.shape[0]*2, 
