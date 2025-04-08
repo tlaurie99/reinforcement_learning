c233abf99b02bc6ef3fee066dae390869f9d5848
Tyler <57071969+tlaurie99@users.noreply.github.com>
2025-04-08 10:34:51 -0400

    Update run_main_hover.py

--- drone_project/run_main_hover.py
+++ drone_project/run_main_hover.py
@@ -14,6 +14,13 @@ from mavsdk.camera import Mode, Setting, Option
 from mavsdk.mission import MissionItem, MissionPlan
 from stable_baselines3.common.policies import ActorCriticPolicy
 from mavsdk.offboard import OffboardError, PositionNedYaw, Attitude
+
+from action_wrapper import NormalizeActions
+from stable_baselines3.common.logger import configure
+from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.env_util import make_vec_env
+
+"""figuring out action selection being none"""
     
 
 def load_yaml_config(file_path):
@@ -28,13 +35,24 @@ if __name__ == "__main__":
 
     hover_config = config['hover_env_config']
     env = DroneHoverEnv(hover_config)
+    env = NormalizeActions(env)
+    vec_env = make_vec_env(lambda: env)
+    norm_env = VecNormalize(vec_env, training=True, norm_obs=True, clip_obs=100, gamma=0.99)
     model = PPO(
         ActorCriticPolicy,
-        env,
+        norm_env,
     )
     sb3_config = config["SB3_config"]
     timesteps = sb3_config.get("timesteps", 1_000_000)
     # asyncio.run(env.compute_state())
+    """---SET SB3 LOGGER---"""
+    train_path = r"/workspace/sb3_results/"
+    if train_path:
+        model_logger = configure(train_path, ['csv'])
+        model.set_logger(model_logger)
+    else:
+        model_logger = None
+        
     model.learn(total_timesteps=timesteps)
 
     # asyncio.run(run())
